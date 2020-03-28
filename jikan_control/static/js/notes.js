1cfda12e7f10db203dbd44df76a3dc6a43e18041
anrarivera <anra.rivera@gmail.com>
2020-03-27 21:15:00 -0400

    Note list page ready. Search needs work deleting

--- jikan_control/static/js/notes.js
+++ jikan_control/static/js/notes.js
@@ -1,5 +1,20 @@
 document.querySelector('#note-list').addEventListener('click', e => {
-  if(e.target.classList.contains('show-note-detail')){
-    $('#note-form-modal').modal('toggle');
+  //Remove
+  if(e.target.classList.contains('remove-event')){
+    const noteRow = e.target.parentElement.parentElement.parentElement;
+    const noteId = noteRow.getAttribute("note-id");
+
+    $.ajax({
+        type: 'POST',
+        url: '/notes/remove',
+        data:{
+	    note_id: noteId,
+	    //where is the csrf token coming from???
+	    csrfmiddlewaretoken: $(input[name=csrfmiddlewaretoken]').val()
+        },
+        success:function(){
+	    //Remove from UI
+	    noteRow.remove();
+        }
+    });
   }
-});
