0d9239e481454de676c6ec57a4ac8d4e9ba3b2ef
anrarivera <anra.rivera@gmail.com>
2020-03-31 00:35:35 -0400

    forms.py for the notes

---
+++ notes/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from .models import Event
+
+# todo Move later to a separate file (or app) that can be used in other apps' view.py
+class DateInput(forms.DateInput):
+  input_type = 'date'
+
+class TimeInput(forms.TimeInput):
+  input_type = 'time'
+
+class EntityIdInput(forms.NumberInput):
+  input_type = 'hidden'
+
+# Used to create the input fields rendered in the template
+class NotesForm(forms.Form):
+  note_id = forms.IntegerField(
+    widget=EntityIdInput(attrs={
+      'id': 'note_id'}),
+    required=False
+    )
+  title = forms.CharField(
+    label='Title', 
+    widget=forms.TextInput(attrs={
+      'id': 'title', 
+      'class': 'form-control'})
+    )
+  description = forms.CharField(
+    label='Description', 
+    widget=forms.Textarea(attrs={
+      'id': 'description', 
+      'class': 'form-control'})
+   )
+  created_date = forms.DateTimeField(
+    label='Created Date', 
+    widget=DateInput(attrs={
+      'id': 'created_date', 
+      'class': 'form-control'}),
+      required=False
+   )
+  event_id = forms.ModelChoiceField( 
+    label='Events',
+    empty_label=None,
+    queryset=Event.objects.all(),
+    widget=forms.Select(attrs={
+      'id':'event_id',
+      'class': 'form-control'})
+   )
