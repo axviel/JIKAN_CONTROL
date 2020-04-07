d3810fc4bee254750fd7963018156b05bc3b7db1
axviel <beniteza777@outlook.com>
2020-04-06 21:32:38 -0400

    Fixed note bugs

--- jikan_control/static/js/notes.js
+++ jikan_control/static/js/notes.js
@@ -1,8 +1,8 @@
 document.querySelector('#note-list').addEventListener('click', e => {
   //Remove
   if(e.target.classList.contains('remove-note')){
-    const noteeRow = e.target.parentElement.parentElement.parentElement;
-    const noteId = noteRow.getAttribute("notee-id");
+    const noteRow = e.target.parentElement.parentElement.parentElement;
+    const noteId = noteRow.getAttribute("note-id");
 
     $.ajax({
         type: 'POST',
@@ -10,12 +10,12 @@ document.querySelector('#note-list').addEventListener('click', e => {
         data:{
 	    note_id: noteId,
 	    //where is the csrf token coming from???
-	    csrfmiddlewaretoken: $(input[name=csrfmiddlewaretoken]').val()
+	    csrfmiddlewaretoken: $('input[name=csrfmiddlewaretoken]').val()
         },
         success:function(){
 	    //Remove from UI
 	    noteRow.remove();
-        }
+        } 
     });
   }
-
+})
