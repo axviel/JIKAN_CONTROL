1b489e071540c3aafd478fc6eb88ac1692876282
Fernando Guzman <fernando.guzman1@upr.edu>
2020-04-08 19:37:19 -0400

    added user form but password change still missing

---
+++ user/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+
+# Used to create the input fields rendered in the template
+class UserForm(forms.Form):
+  username = forms.CharField(
+    label='Username', 
+    widget=forms.TextInput(attrs={
+      'id': 'username', 
+      'class': 'form-control'})
+    )
+  first_name = forms.CharField(
+    label='First Name', 
+    widget=forms.TextInput(attrs={
+      'id': 'first_name', 
+      'class': 'form-control'})
+    )
+  last_name = forms.CharField(
+    label='Last Name', 
+    widget=forms.TextInput(attrs={
+      'id': 'last_name', 
+      'class': 'form-control'})
+    )
+  email = forms.CharField(
+    label='Email', 
+    widget=forms.TextInput(attrs={
+      'id': 'email', 
+      'class': 'form-control'})
+    )
+  password = forms.CharField(
+    label='Password', 
+    widget=forms.PasswordInput(attrs={
+      'id': 'password', 
+      'class': 'form-control'}),
+      required=False
+    )
